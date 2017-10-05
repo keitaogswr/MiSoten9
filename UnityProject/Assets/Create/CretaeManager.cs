8c9978c8cc243ee306a836f4a3d985deba8849fd
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-10-05 12:55:26 +0900

    create manager

--- UnityProject/Assets/Create/CretaeManager.cs
+++ UnityProject/Assets/Create/CretaeManager.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class CretaeManager : MonoBehaviour {
     public List<GameObject> prefab;
+    static private List<GameObject> obj; 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake() {
+        for (int i = 0; i < prefab.Count; i++) {
+            //  オブジェクトがなければ生成。
+            if (GameObject.Find(prefab[i].name) == null) {
+                obj.Add(Instantiate(prefab[i]) as GameObject);
+                obj[i].name = prefab[i].name;
+            }
+        }
+    }
+
+    GameObject getObject(string name) {
+        for (int i = 0; i < prefab.Count; i++) {
+            if (name == prefab[i].name) {
+                return prefab[i];
+            }
+        }
+
+        return null;
+    }
 }
