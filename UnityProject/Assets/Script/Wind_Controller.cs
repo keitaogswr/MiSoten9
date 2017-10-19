bd3e3bbd89bb8929fbdaf42734b453e589245740
Kuroda Masaki <rjsyc666@yahoo.co.jp>
2017-10-19 16:51:55 +0900

    ノーツ叩いた時の動きの追加、適当にWindZoneの動かすスクリプト追加（WindZone使わないで風の表現をやる可能性あり）

---
+++ UnityProject/Assets/Script/Wind_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Rotate(new Vector3(0, 0.1f, 0));
+		
+	}
+}
