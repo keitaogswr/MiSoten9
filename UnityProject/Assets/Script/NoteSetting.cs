eb01e45ca28ca11ca0806fc39d7fb594254c562b
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-10-26 14:16:03 +0900

    追加分

---
+++ UnityProject/Assets/Script/NoteSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSetting : MonoBehaviour {
+    public string FindCameraName;
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Canvas>().worldCamera = GameObject.Find(FindCameraName).GetComponent<Camera>();
+	}
+}
