6fd11fef8330f6085eed9c87b985d695352bb074
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-11-09 14:17:27 +0900

    スクリプト追加

---
+++ UnityProject/Assets/CameraChild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChild : MonoBehaviour {
+    public GameObject target = null;
+    public Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		if (target != null) {
+            GameObject obj = GameObject.Find(target.name);
+            if (obj != null) {
+                this.transform.parent = obj.transform;
+                this.transform.localPosition = pos;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
