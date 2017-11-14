32a94c3cbe5e7d324009c6e3eaa66673a2ea00bb
DESKTOP-T8LI50V\Sigurd <s40376sigurd@gmail.com>
2017-11-14 18:07:47 +0900

    resultテクスチャ素材追加

---
+++ UnityProject/Assets/resultBG.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resultBG : MonoBehaviour {
+
+    float flower01_PosY;
+    public GameObject flower01;
+
+	// Use this for initialization
+	void Start () {
+        flower01 = GameObject.Find("UI_result_2");
+        flower01.transform.localPosition = new Vector2(0,20);
+        flower01_PosY = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        flower01.transform.localPosition = new Vector2(0, 20);
+    }
+}
