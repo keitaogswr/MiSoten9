5413a6285dc47b40bc8b5b4fce088df72905ad17
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-11-16 10:38:42 +0900

    uv移動

---
+++ UnityProject/Assets/uv.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class uv : MonoBehaviour {
+    [Range(-1.0f, 1.0f)]
+    public float move_x = 0.0f;
+    [Range(-1.0f, 1.0f)]
+    public float move_y = 0.0f;
+    private RawImage texture;
+	// Use this for initialization
+	void Start () {
+        texture = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        texture.uvRect = new Rect(texture.uvRect.x + move_x, texture.uvRect.y + move_y, texture.uvRect.width, texture.uvRect.height);
+	}
+}
