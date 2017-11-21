0bf9aab7587668be1a9ec439afdd201e11895d5d
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-11-21 16:54:50 +0900

    Tornado更新

--- UnityProject/Assets/TornadoManager.cs
+++ UnityProject/Assets/TornadoManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class TornadoManager : MonoBehaviour {
 
     public int simultaneous = 3;        //  同時に生成できる数。
-    public float createRange = 50.0f;   //  生成範囲。
+    public float createTime = 10.0f;
     public List<GameObject> tornadoPrefab;  //  生成したいトルネードの種類を入れておく。
-
+    [Range(0f, 300.0f)]
+    public float createRange = 0.0f;
     private int tornadoNum = 0;
+    private float timeCount = 0.0f;
 
     // Use this for initialization
     void Start () {
@@ -16,6 +18,13 @@ public class TornadoManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        timeCount += Time.deltaTime;
+
+        if (timeCount >= createTime) {
+            timeCount = 0.0f;
+            CreateTornado(0);
+        }
+
         if (Input.GetKeyDown(KeyCode.Return)) {
             CreateTornado(0);
         }
@@ -25,7 +34,7 @@ public class TornadoManager : MonoBehaviour {
         for (int i = 0; i < tornado.Length; i++) {
             if (tornadoNum < simultaneous - 1) {
                 if (tornado[i] < tornadoPrefab.Count) {
-                    GameObject obj = Instantiate(tornadoPrefab[tornado[i]]);
+                    GameObject obj = Instantiate(tornadoPrefab[tornado[i]], new Vector3(transform.position.x + Random.Range(-createRange, createRange), transform.position.y, transform.position.z + Random.Range(-createRange, createRange)), Quaternion.identity);
                     obj.name = tornadoPrefab[tornado[i]].name;
                     CountUp();
                 }
