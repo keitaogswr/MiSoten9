338f1562985e24c4e52dbaef24c5cedd11bc484e
fukasawayuuta <as.fukasawayuuta@gmail.com>
2017-11-21 18:05:17 +0900

    追加分。円のコライダー消しました。

--- UnityProject/Assets/Script/CheckPoint.cs
+++ UnityProject/Assets/Script/CheckPoint.cs
@@ -10,12 +10,8 @@ public class CheckPoint : MonoBehaviour {
     private float elapsedTime = 0.0f;
     private Es.InkPainter.Sample.Paint brush;
     private bool drawStart = false;
-    private SphereCollider childColl;
     // Use this for initialization
     void Start () {
-        childColl = transform.Find("DrawField").GetComponent<SphereCollider>();
-        if (childColl != null)
-            childColl.enabled = false;
         addScal = drawRange / sec;
         brush = this.GetComponent<Es.InkPainter.Sample.Paint>();
         brush.getBrush().Scale = 0.0f;
@@ -23,13 +19,6 @@ public class CheckPoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (childColl.enabled == true) {
-            if (elapsedTime < sec) {
-                elapsedTime += Time.deltaTime;
-                childColl.radius = addScal * elapsedTime;
-            }
-        }
-
         if (drawStart) {
             if (brush.getBrush().Scale > drawRange) {
                 drawStart = false;
@@ -41,7 +30,6 @@ public class CheckPoint : MonoBehaviour {
 
     void OnTriggerStay(Collider other) {
         if (other.gameObject.tag == "Player") {
-            childColl.enabled = true;
             drawStart = true;
         }
     }
