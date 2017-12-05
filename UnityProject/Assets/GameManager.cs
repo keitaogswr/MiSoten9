c9f7f61047a828bbe3e65f4d24d7e8435714b55e
Kuroda Masaki <rjsyc666@yahoo.co.jp>
2017-12-05 16:31:31 +0900

    マップのスタートまでの停止処理（エラー部分を出さない方法は知らない）それとミニマップにプレイヤーが少し分かりやすく表示

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject Player_2P;
+    public GameObject Terrain;
+    public GameObject Game_UI;
+    public GameObject Game_UI_2P;
+
+    public float WaitTimer;
+    private float Timer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Player.GetComponent<Player>().enabled = false;
+        Player_2P.GetComponent<Player>().enabled = false;
+        Terrain.GetComponent<TerraScript>().enabled = false;
+        Game_UI.GetComponent<Notes_C>().enabled = false;
+        Game_UI_2P.GetComponent<Notes_C>().enabled = false;
+
+        GameObject.Find("Player_UI_1/Text").GetComponent<Text>().text = "Wait";
+        GameObject.Find("Player_UI_2/Text").GetComponent<Text>().text = "Wait";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Timer += Time.deltaTime;
+
+        GameObject.Find("Player_UI_1/Text").GetComponent<Text>().text = "Wait" + Timer;
+        GameObject.Find("Player_UI_2/Text").GetComponent<Text>().text = "Wait" + Timer;
+
+        if (Timer > WaitTimer)
+        {
+            Player.GetComponent<Player>().enabled = true;
+            Player_2P.GetComponent<Player>().enabled = true;
+            Terrain.GetComponent<TerraScript>().enabled = true;
+            Game_UI.GetComponent<Notes_C>().enabled = true;
+            Game_UI_2P.GetComponent<Notes_C>().enabled = true;
+
+            Destroy(this.gameObject);
+
+            GameObject.Find("Player_UI_1/Text").GetComponent<Text>().text = "Start";
+            GameObject.Find("Player_UI_2/Text").GetComponent<Text>().text = "Start";
+
+            Destroy(GameObject.Find("Player_UI_1/Text").gameObject,1);
+            Destroy(GameObject.Find("Player_UI_2/Text").gameObject,1);
+        }
+	}
+}
---
+++ UnityProject/Assets/MiniMapIconMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapIconMover : MonoBehaviour {
+
+    public GameObject Player;
+    public Vector3 Player1stPos;
+
+
+	// Use this for initialization
+	void Start () {
+        Player1stPos = Player.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 MoveVec = Player.transform.localPosition - Player1stPos;
+
+        MoveVec /= 10;
+        MoveVec *= 4;
+       
+        this.transform.localPosition = new Vector3((MoveVec.x), -98, 0);
+        this.transform.localPosition += new Vector3(0, (MoveVec.z), 0);
+
+    }
+}
