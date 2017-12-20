bd71db1a508c7ac2ae971e19c4ded6d8c137a911
Kuroda Masaki <rjsyc666@yahoo.co.jp>
2017-12-20 15:12:27 +0900

    タイムアウトでのノーツの停止および生産停止、スタート時のロゴのアニメーションちっくなものを追加、UIのPrefab化

--- UnityProject/Assets/Script/Note2.cs
+++ UnityProject/Assets/Script/Note2.cs
@@ -6,16 +6,25 @@ public class Note2 : MonoBehaviour {
 
     public float Note_Speed = 10;
 
+    private Timer timer;
+
     // Use this for initialization
     void Start()
     {
-
+        timer = GameObject.Find("Player_UI_1/TimerFrame/Timer").GetComponent<Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += Vector3.right * Note_Speed * Time.deltaTime;
+        if(timer.GetTimeOverFlag())
+        {
+        }
+        else
+        {
+            this.transform.position += Vector3.right * Note_Speed * Time.deltaTime;
+        }
+        
     }
 
     public void DestroyObj()
---
+++ UnityProject/Assets/StartLogoAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLogoAnim : MonoBehaviour {
+
+    private bool bFadeout = false;
+    private float AnimTime = 0;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        AnimTime += Time.deltaTime;
+		if(bFadeout == false)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, Mathf.Lerp(this.transform.localPosition.y, 0, AnimTime * 2), this.transform.localPosition.z);
+            
+            if(AnimTime > 1.0f)
+            {
+                bFadeout = true;
+            }
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, AnimTime * 60));
+            transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+
+            if(transform.localScale.x < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+}
