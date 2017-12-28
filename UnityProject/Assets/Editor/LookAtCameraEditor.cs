3561af558bc068097323e4fc8af0cf3ca6516cd9
KojiNomura7117 <kojinomura7117@gmail.com>
2017-12-28 20:11:49 +0900

    会場にバックスクリーン配置

---
+++ UnityProject/Assets/Editor/LookAtCameraEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Camera))]
+public class LookAtCameraEditor : Editor
+{
+	Vector3 value;
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		EditorGUILayout.BeginHorizontal();
+		value = EditorGUILayout.Vector3Field("target", value);
+		if (GUILayout.Button("Look", GUILayout.MaxWidth(60)))
+		{
+			Camera c = target as Camera;
+			Undo.RecordObject(c, "Camera LookAt");
+			c.transform.rotation = Quaternion.LookRotation(value - c.transform.position);
+			EditorUtility.SetDirty(target);
+		}
+		EditorGUILayout.EndHorizontal();
+	}
+}
