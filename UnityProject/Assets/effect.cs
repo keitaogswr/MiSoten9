f3474a728ded4ceecfd423b4fe18f5c50662955f
DESKTOP-PC896L0\yuuta <as.fukasawayuuta@gmail.com>
2018-01-05 23:48:31 +0900

    prefabめちゃふえるよ

---
+++ UnityProject/Assets/effect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect : MonoBehaviour {
+
+    [SerializeField]
+    private float validTime = 0.0f;
+    private float workTime = 0.0f;
+
+    public GameObject createObj = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        workTime += Time.deltaTime;
+        if (workTime >= validTime) {
+            Destroy(this.gameObject);
+
+            if (createObj != null) {
+                Instantiate(createObj);
+            }
+        }
+    }
+}
